940f5e6b66285f5b9afd53721bdcbd29a2916b23
Administrator <434754018@qq.com>
2017-06-23 23:55:52 +0800

    this is a test about github!

---
+++ test/src/com/cn/test/Main.java
@@ -0,0 +1,14 @@
+package com.cn.test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello,I'm coming,now!");
+	}
+
+}
